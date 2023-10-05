ad942c6a225a1e3e23605193892f358cdee6017e
= <tristanmayofreire@gmail.com>
2023-10-05 17:03:55 +0200

    updated project texts and added golang as opt

--- src/i18n/ui.tsx
+++ src/i18n/ui.tsx
@@ -28,6 +28,9 @@ export const SkillList = {
   ts: {
     txt: "Typescript",
   },
+  go: {
+    txt: "Golang",
+  },
   // Front
   tailwind: {
     txt: "Tailwindcss",
@@ -106,86 +109,120 @@ export const SkillList = {
 };
 import type { ProjectData } from "../components/ui/Project";
 export const Projects: ProjectData[] = [
+  //   {
+  //     title: "Ridaly Digital",
+  //     category: Categories.website,
+  //     tecnologies: [
+  //       SkillList.react,
+  //       SkillList.ts,
+  //       SkillList.tailwind,
+  //       SkillList.astro,
+  //       SkillList.html,
+  //       SkillList.css,
+  //     ],
+  //     desc: [
+  //       `Ridaly's Marketing Agency is a Madrid-based company that specializes
+  // in marketing, design, and technology. They offer a wide range of
+  // services to help businesses of all sizes connect with their audience,
+  // including strategy development, marketing, technology, content
+  // creation, and design. As the designer and developer of the website,
+  // you can take pride in creating a platform that showcases Ridaly's
+  // unique capabilities and expertise.`,
+  //       `Their strategy services include data analysis, user persona mapping,
+  // and audits to help businesses develop unique recommendations that will
+  // reduce expenses and increase income. The marketing services they offer
+  // encompass all digital and offline marketing channels, including social
+  // media, SEO, SEM, and conversion optimization. Their technology
+  // services include front-end and back-end development, app development,
+  // and integrations. They also offer content creation services such as
+  // content strategy, copywriting, audiovisual production, and social
+  // media management.`,
+  //       `Ridaly's Marketing Agency places great emphasis on design, including
+  // branding, UX/UI, and creation of assets. They pride themselves on
+  // creating unforgettable visual identities that combine seamlessly with
+  // user experiences that enchant, engage, and convert.`,
+  //       `With Ridaly's Marketing Agency, businesses can focus on their core
+  // operations while leaving the marketing, design, and technology tasks
+  // to the experts.`,
+  //     ],
+  //     btns: [
+  //       {
+  //         t: "View on Github",
+  //         href: "/",
+  //       },
+  //     ],
+  //     imgs: {
+  //       alt: "Testing",
+  //       desktop: "project_showcase_ridaly-desktop.jpeg",
+  //       tablet: "project_showcase_ridaly-tablet.jpeg",
+  //       phone: "project_showcase_ridaly-phone.jpeg",
+  //     },
+  //   },
+
   {
-    title: "Ridaly Digital",
+    title: "Ridaly Hosting",
     category: Categories.website,
     tecnologies: [
-      SkillList.react,
       SkillList.ts,
+      SkillList.next,
+      SkillList.mysql,
       SkillList.tailwind,
-      SkillList.astro,
-      SkillList.html,
-      SkillList.css,
+      SkillList.prisma,
+      SkillList.trpc,
+      SkillList.react,
     ],
     desc: [
       `Ridaly's Marketing Agency is a Madrid-based company that specializes
-in marketing, design, and technology. They offer a wide range of
-services to help businesses of all sizes connect with their audience,
-including strategy development, marketing, technology, content
-creation, and design. As the designer and developer of the website,
-you can take pride in creating a platform that showcases Ridaly's
-unique capabilities and expertise.`,
-      `Their strategy services include data analysis, user persona mapping,
-and audits to help businesses develop unique recommendations that will
-reduce expenses and increase income. The marketing services they offer
-encompass all digital and offline marketing channels, including social
-media, SEO, SEM, and conversion optimization. Their technology
-services include front-end and back-end development, app development,
-and integrations. They also offer content creation services such as
-content strategy, copywriting, audiovisual production, and social
-media management.`,
-      `Ridaly's Marketing Agency places great emphasis on design, including
-branding, UX/UI, and creation of assets. They pride themselves on
-creating unforgettable visual identities that combine seamlessly with
-user experiences that enchant, engage, and convert.`,
-      `With Ridaly's Marketing Agency, businesses can focus on their core
-operations while leaving the marketing, design, and technology tasks
-to the experts.`,
+  in marketing, design, and technology. They offer a wide range of
+  services to help businesses of all sizes connect with their audience,
+  including strategy development, marketing, technology, content
+  creation, and design.`,
+      `I developed the backend hosting site completely on my own (including the design) using the T3 Stack (Typescript, Nextjs & Tailwindcss, read more about it here -> create.t3.gg). The site integrates with stripe to handle transations & caches subscription, payment and receipt information on a mysql database to avoid saturating the stripe api.`,
+      `In addition, the website is fully i18n compatible and is already available in spanish & english.`,
+      `Unfortunatly, the code is not freely accesible, I have provided a link to an old version of the code made for my CS50 Final Project. However the current site is much more advanced. If you would like to take a look at it message me and I can give you access.`,
     ],
     btns: [
       {
-        t: "View on Github",
-        href: "/",
+        t: "Github (old)",
+        href: "https://github.com/Oudwins/subscriptioner-template",
+      },
+      {
+        t: "View site",
+        href: "https://app.ridaly.com/",
       },
     ],
     imgs: {
-      alt: "Testing",
-      desktop: "project_showcase_ridaly-desktop.jpeg",
-      tablet: "project_showcase_ridaly-tablet.jpeg",
-      phone: "project_showcase_ridaly-phone.jpeg",
+      alt: "Ridaly Digital Website Hosting Application",
+      desktop: "project_showcase_ridalyhosting-desktop.jpeg",
+      tablet: "project_showcase_ridalyhosting-tablet.jpeg",
+      phone: "project_showcase_ridalyhosting-phone.jpeg",
     },
   },
   {
     title: "Natours Travel",
-    category: Categories.website,
+    category: Categories.api,
     tecnologies: [SkillList.nodejs, SkillList.express, SkillList.mongodb],
     desc: [
-      `Natours es una página de venta de tours. De experiencias de todo tipo.
-    He desarollado el backend usando el patron MVC. Usando json web tokens
-    para autentificar a los usuarios.`,
-      `EL servidor, además de renderizar la web, también tiene una extensa
-    API, que permite realizar todas las operaciones comunes de CRUD
-    (Crear, leer, actualizar y borrar) con cado uno de los recursos.
-    Cuenta con 4 recursos, los usuarios, para los cuales he implementado
-    los procesos de creación de cuenta, inicio de sesión, recuperación de
-    contraseña vía envio de código a su email...`,
-      `Luego está el recurso de Tours, para el que las operaciones de CRUD
-    están limitadas según el rol del usuario. Por otro lado también he
-    implementado la opción de dejar reviews y reservar tours. La
-    aplicación no está acabada, sobre todo el front end que es algo de lo
-    que no me he hecho cargo.`,
+      `Natours is a tour selling website. Offering experiences of all kinds.
+        I developed the backend using the MVC pattern. Utilizing JSON web tokens
+        for user authentication.`,
+      `The server, in addition to rendering site, also has an extensive
+        API that allows performing all common CRUD operations
+        (Create, Read, Update, Delete) for each of the 4 resources: users, tours, reviews and bookings.`,
+      `For the users resource, I implemented extensive auth such as: account creation, login, password recovery via a recovery code and much more`,
+      `On the other hand, for the Tours resource, I implemented auth checks to limit crud operations based on the user's role. And the option to leave reviews and book tours.`,
     ],
     btns: [
       {
-        t: "View on Github",
-        href: "/",
+        t: "View Github",
+        href: "https://github.com/Oudwins/natours",
       },
     ],
     imgs: {
-      alt: "Testing",
-      desktop: "project_showcase_ridaly-desktop.jpeg",
-      tablet: "project_showcase_ridaly-tablet.jpeg",
-      phone: "project_showcase_ridaly-phone.jpeg",
+      alt: "Natours API project featured image",
+      desktop: "project_showcase_natours-desktop.jpg",
+      // tablet: "project_showcase_ridaly-tablet.jpeg",
+      // phone: "project_showcase_ridaly-phone.jpeg",
     },
   },
   {
@@ -201,27 +238,32 @@ to the experts.`,
       SkillList.html,
     ],
     desc: [
-      `Zwary es una página web para un restaurante de alta gama. Está
-    desarrollada y diseñada íntegramente por mi y es 100% responsive.`,
-      `La web tiene un diseño único y especial pues era de vital importancia
-    que representara la particularidad del restaurante a través de su
-    diseño. En la página de inicio se ha creado una experiencia de usuario
-    exclusiva ubicando en su centro un elemento visual importante.
-    Mientras que en el resto de las páginas se refleja un estilo sencillo
-    pero elegante que está perfectamente alineado con las intenciones del
-    restaurante.`,
+      `Zwary is a website for a high-end restaurant. It is
+        developed and designed entirely by me and is 100% responsive. The focus is on making a great design with amazing custom animations. The screenshot does not do it justice, so please visit the site to see it in action.`,
+      `The website has a unique and special design because it was crucial
+        to represent the restaurant's uniqueness through its
+        design. On the homepage, an exclusive user experience has been created
+        by placing an important visual element in its center.
+        Meanwhile, on the rest of the pages, a simple yet elegant style is reflected
+        that aligns perfectly with the restaurant's intentions.`,
+      `The site was developed using plan HTML, CSS & JS. Using the Gsap animation library to achieve some of the amazing animations you can see on screen.`,
     ],
     btns: [
       {
         t: "View on Github",
-        href: "/",
+        href: "https://github.com/Oudwins/zwary",
+      },
+      {
+        t: "View Site",
+        href: "https://www.zwary.tristanmayo.com/",
       },
     ],
     imgs: {
-      alt: "Testing",
-      desktop: "project_showcase_ridaly-desktop.jpeg",
-      tablet: "project_showcase_ridaly-tablet.jpeg",
-      phone: "project_showcase_ridaly-phone.jpeg",
+      alt: "Website for the Zwary Restaurant",
+      desktop: "project_showcase_zwary-desktop.jpeg",
+      tablet: "project_showcase_zwary-tablet.jpeg",
+      phone: "project_showcase_zwary-phone.jpeg",
     },
+    disableScrollImage: true,
   },
 ];
